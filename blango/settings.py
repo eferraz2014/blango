a1886726c047c5095a5831e02ad446460acc795a
Edilson Ferraz <eferraz2014@gmail.com>
2024-10-26 17:49:35 +0100

    crispy tags

--- blango/settings.py
+++ blango/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = 'blango.wsgi.application'
 
